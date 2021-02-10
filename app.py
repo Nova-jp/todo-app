ea17c4c65c116c55e8154d099ca386c7196b4a5c
Nova-jp <hiroto.nova@gmail.com>
2021-02-10 14:15:00 +0900

    extended the limit of title size

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(30), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     header = db.Column(db.String(30), nullable=False)
     detail = db.Column(db.String(100))
     due = db.Column(db.DateTime, nullable=False)
