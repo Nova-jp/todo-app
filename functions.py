784583153ca1e89152c5b984b7ec4cb5d1c1cbc1
Nova-jp <hiroto.nova@gmail.com>
2021-02-10 14:47:25 +0900

    fixed functions

--- functions.py
+++ functions.py
@@ -2,10 +2,8 @@ def shrinktitle(name):
     cnt = 0
     now = 0
     for i in name:
-        if i == '/':
-            cnt = now
-        now += 1
-    if cnt != 0:
-      cnt+=1
+      now += 1
+      if i == '/':
+         cnt = now
     b = name[cnt:now]
     return b
